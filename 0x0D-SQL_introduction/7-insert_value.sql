b0c843a6f5b9640383cf0a20a4906e44350461b7
Nyangimo <rebeccahnyangige9@gmail.com>
2023-08-15 13:25:53 -0700

    6-list_values.sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,6 @@
+-- This query inserts a new row into the table "first_table" in the database "hbtn_0c_0"
 
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
+
+INSERT INTO hbtn_0c_0.first_table (id, name)
+VALUES (89, 'Best School');
+
