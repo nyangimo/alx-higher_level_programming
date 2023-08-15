be7619c42d83a347c7d5ecde26e3bbbb5fe73548
Nyangimo <rebeccahnyangige9@gmail.com>
2023-08-15 13:36:47 -0700

    13-change_class.sql

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,1 +1,5 @@
-DELETE FROM second_table WHERE score <= 5;
+-- This query deletes all records with a score less than or equal to 5 in the table "second_table"
+
+DELETE FROM hbtn_0c_0.second_table
+WHERE score <= 5;
+
