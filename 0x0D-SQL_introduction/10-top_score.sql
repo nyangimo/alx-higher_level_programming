324fc76ef32716d9a27fc1d2fa66acad5a7ce246
Nyangimo <rebeccahnyangige9@gmail.com>
2023-08-15 13:29:52 -0700

    6-list_values.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,6 @@
+-- This query retrieves all records from the table "second_table" of the database "hbtn_0c_0"
 
-SELECT score, name FROM second_table ORDER BY score DESC;
+SELECT score, name
+FROM hbtn_0c_0.second_table
+ORDER BY score DESC;
+
