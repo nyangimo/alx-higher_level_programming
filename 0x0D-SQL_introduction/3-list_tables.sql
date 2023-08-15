d8dfe41e416e9d5d4dd785d1f4138567f381ae15
Nyangimo <rebeccahnyangige9@gmail.com>
2023-08-15 13:20:33 -0700

    3-list_tables.sql

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,4 +1,7 @@
 -- This SQL script lists all the tables of the specified datbase.
 
-SHOW TABLES FROM DATABASE();
+SELECT TABLE_NAME
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'your_database_name';
+
 
--- 0x0D-SQL_introduction/5-full_tabled.sql
+++
@@ -1,6 +0,0 @@
--- This SQL script retrieves and prints the full description of the table.
-
-SELECT TABLE_NAME, CREATE_TABLE
-FROM information_schema.TABLES
-WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'first_table';
-
