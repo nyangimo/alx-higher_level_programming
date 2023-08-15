763936e25fa5c75924fcab2aa2ece444a70caadb
Nyangimo <rebeccahnyangige9@gmail.com>
2023-08-15 13:33:49 -0700

    6-list_values.sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,6 @@
+-- This query updates the score of the person named "Bob" to 10 in the table "second_table"
 
-UPDATE second_table
+UPDATE hbtn_0c_0.second_table
 SET score = 10
 WHERE name = 'Bob';
 
