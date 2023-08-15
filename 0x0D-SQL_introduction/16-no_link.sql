14c81139c70614625001e486af6bd735ea55bcd7
Nyangimo <rebeccahnyangige9@gmail.com>
2023-08-15 13:45:16 -0700

    13-change_class.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,9 @@
+-- This query retrieves records from the table "second_table" of the database "hbtn_0c_0"
+-- The query excludes rows without a name value
+-- The results display the score and the name, ordered by descending score
+
 SELECT score, name
-FROM second_table
-HAVING name IS NOT NULL
+FROM hbtn_0c_0.second_table
+WHERE name IS NOT NULL
 ORDER BY score DESC;
+
