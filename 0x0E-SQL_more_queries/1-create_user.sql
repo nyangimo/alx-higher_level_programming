ecce0e72024f133440d88b7716b4b46bbce353a1
Nyangimo <rebeccahnyangige9@gmail.com>
2023-08-16 13:02:46 -0700

    3-force_name.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,8 +1,5 @@
 --A script that creates the user_0d_1 with all privilege
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
-IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * 
-TO 'user_0d_1'@'localhost';
-
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,3 @@
 --A script that creates the table force_name
 
-CREATE TABLE IF NOT EXISTS 'force_name'('id' INT, 'name' VARCHAR(256) NOT NULL);
-
---A script that creates a table unique_id
+CREATE TABLE IF NOT EXISTS force_name(id INT, name VARCHAR(256) NOT NULL);
