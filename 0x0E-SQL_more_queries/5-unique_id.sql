f3bd9130c5041cb701d79f50eaadb594a32adda7
Nyangimo <rebeccahnyangige9@gmail.com>
2023-08-25 03:31:14 -0700

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,8 +1,4 @@
---A script that creates the table hbtn_0d_usa with table states
+-- A script that creates the table unique_id on your MySQL server
+-- creates a table
 
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
-CREATE TABLE IF NOT EXISTS 'hbtn_0d_usa'.'states' (PRIMARY KEY ('id'),
-'id' INT   
-NOT NULL AUTO_INCREMENT,
-'name' VARCHAR(256) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,10 +1,7 @@
---A script that creates a database hbtn_0d_usa with the table cities
-
-CREATE A DATABASE IF NOT EXISTS
-CREATE A TABLE IF NOT EXISTS 'hbtn_0d_usa'.'cities' (PRIMARY KEY('id'),
-'id'  INT  NOT NULL AUTO_INCREMENT,
-'state_id' INT   NOT NULL,
-'name'  VARCHAR(256) NOT NULL,
-FOREIGN KEY('state_id')
-REFERENCES 'hbtn_0_usa'.'states'('id')
-);
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use a database
+USE hbtn_0d_usa;
+-- creates a table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
